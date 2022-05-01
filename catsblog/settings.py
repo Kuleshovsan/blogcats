9522fe69583e1cc1c38dca1d2e708723abb21249
ehmatthes <sanya-kuleshov@ya.ru>
2022-05-01 11:02:54 +0300

    Ready for deployment to herok

--- catsblog/settings.py
+++ catsblog/settings.py
@@ -124,7 +124,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = '/users/login/'
-
+LOGIN_URL = 'users:login'
 import django_heroku
 django_heroku.settings(locals())
